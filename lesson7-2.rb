59282aa9694287a7e1b38ca407cfb1a976b058ac
EC2 Default User <ec2-user@ip-172-31-16-146.ec2.internal>
2022-08-26 08:20:36 +0000

    7章【繰り返し処理を学ぼう】

---
+++ lesson7-2.rb
@@ -0,0 +1,26 @@
+puts "計算をはじめます
+何回計算を繰り返しますか？"
+times=gets.to_i
+
+i=1
+
+while i <=times do
+
+ puts "#{i}回目の計算"
+puts "2つの値を入力してください"
+a=gets.to_i
+b=gets.to_i
+
+puts"a=#{a}"
+puts"b=#{b}"
+
+puts"a+b=#{a+b}"
+puts"a-b=#{a-b}"
+puts"a*b=#{a*b}"
+puts"a/b=#{a / b}"
+
+i += 1
+
+end
+
+puts 
